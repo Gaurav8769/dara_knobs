115b81dbf9968f95a41d5d01891f1f9e6e9ac5be
Gaurav8769 <99468457+Gaurav8769@users.noreply.github.com>
2023-06-30 13:58:11 +0530

    Add files via upload

---
+++ Pinecone/main.py
@@ -0,0 +1,16 @@
+import os
+import faiss
+import torch
+import skimage
+import requests
+import pinecone
+import numpy as np
+import pandas as pd
+from PIL import Image
+from io import BytesIO
+import IPython.display
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+import pandas as pd
+import numpy as np
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
